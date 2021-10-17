6c46bcd325cf2efcc6f287742d41526bfe8c1b29
shrivastavpiyush0423 <piyush.srivastava_cs19@gla.ac.in>
2021-10-17 21:47:07 +0530

    first commit

---
+++ Assignment-14/counter.js
@@ -0,0 +1,13 @@
+var data=0;
+        document.getElementById("counting").innerText=data;
+        function increment()
+        {
+            data=data+1;
+            document.getElementById("counting").innerText=data;
+        }
+        function decrement()
+        {
+            if(data>0)
+            data=data-1;
+            document.getElementById("counting").innerText=data;
+        }
